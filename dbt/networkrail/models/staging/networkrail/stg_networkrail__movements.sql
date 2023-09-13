01dcb2b6605dbb132d9ad0e1b222e8dc9912bf5f
phakawatfong <phakawatfong@gmail.com>
2023-09-13 23:09:50 +0700

    add my first staging_model SQL file

---
+++ dbt/networkrail/models/staging/networkrail/stg_networkrail__movements.sql
@@ -0,0 +1,60 @@
+with
+
+source as (
+
+    select * from {{ source('networkrail', 'movements') }}
+
+)
+
+, renamed_recasted as (
+
+    select 
+        event_type,
+        gbtt_timestamp,
+        DATETIME(gbtt_timestamp, "Europe/London") as gbtt_timestamp_utc,
+        original_loc_stanox,
+        DATETIME(planned_timestamp, "Europe/London") as planned_timestamp_utc,
+        timetable_variation,
+        original_loc_timestamp,
+        current_train_id,
+        delay_monitoring_point,
+        next_report_run_time,
+        reporting_stanox,
+        DATETIME(actual_timestamp, "Europe/London") as actual_timestamp_utc,
+        correction_ind,
+        event_source,
+        train_file_address,
+        platform,
+        division_code,
+        train_terminated,
+        train_id,
+        offroute_ind,
+        variation_status,
+        train_service_code,
+        toc_id,
+        loc_stanox,
+        auto_expected,
+        direction_ind,
+        route,
+        planned_event_type,
+        next_report_stanox,
+        line_ind,
+    from source
+
+)
+
+, final as (
+
+    -- Your code here
+    select
+        event_type,
+        actual_timestamp_utc,
+        event_source,
+        train_id,
+        variation_status,
+        toc_id 
+    from renamed_recasted
+
+)
+
+select * from final
