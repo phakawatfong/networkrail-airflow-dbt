6c79eabef63d2c0f40ecc0c54658f729f01c4200
phakawatfong <phakawatfong@gmail.com>
2023-09-16 18:24:25 +0700

    add curated layer into dbt project

---
+++ dbt/networkrail/models/curated/networkrail/crt_networkrail_fact_movements.sql
@@ -0,0 +1,18 @@
+with pst_networkrail_movements AS (
+    SELECT * FROM {{ ref('pst_networkrail_fact_movements') }}
+)
+
+, final AS (
+
+    SELECT
+            event_type,
+            actual_timestamp_utc,
+            event_source,
+            train_id,
+            variation_status,
+            toc_id
+    FROM pst_networkrail_movements
+)
+
+
+SELECT * FROM final
