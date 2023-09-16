f4e7bb77a83998853b0b17a14a8514d25274f166
phakawatfong <phakawatfong@gmail.com>
2023-09-16 17:52:41 +0700

    modify movements table staging model

--- dbt/networkrail/models/staging/networkrail/stg_networkrail__movements.sql
+++ dbt/networkrail/models/staging/networkrail/stg_networkrail__movements.sql
@@ -45,14 +45,38 @@ source as (
 
 , final as (
 
-    -- Your code here
+
     select
         event_type,
+        gbtt_timestamp,
+        gbtt_timestamp_utc,
+        original_loc_stanox,
+        planned_timestamp_utc,
+        timetable_variation,
+        original_loc_timestamp,
+        current_train_id,
+        delay_monitoring_point,
+        next_report_run_time,
+        reporting_stanox,
         actual_timestamp_utc,
+        correction_ind,
         event_source,
+        train_file_address,
+        platform,
+        division_code,
+        train_terminated,
         train_id,
+        offroute_ind,
         variation_status,
-        toc_id 
+        train_service_code,
+        toc_id,
+        loc_stanox,
+        auto_expected,
+        direction_ind,
+        route,
+        planned_event_type,
+        next_report_stanox,
+        line_ind,
     from renamed_recasted
 
 )
